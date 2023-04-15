859e4d22290ca1e44610802805a8e573198dd4b8
Алексей Пронин <61554995+dlbbv@users.noreply.github.com>
2023-04-15 21:22:23 +0300

    Update settings.py

--- nested_menu/testtask/testtask/settings.py
+++ nested_menu/testtask/testtask/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c)!_1#ig622sy-$3=ne2fm1gzb39%%3=dhhz#s9rn-ihmi&(uo'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
